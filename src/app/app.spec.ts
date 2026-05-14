74a42ac87f78a4a2e00e30eed339d961355e94ce
Adam Matoľák <adam.matolak@outlook.sk>
2026-05-14 19:18:55 +0200

    fixed test stage

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
+import { KeycloakService } from './keycloak.service';
 
 describe('App', () => {
+  let mockKeycloakService: any;
+
   beforeEach(async () => {
+    mockKeycloakService = {
+      getUsername: () => 'test-user',
+      hasRole: (role: string) => false,
+      logout: () => Promise.resolve()
+    };
+
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideRouter([]),
+        { provide: KeycloakService, useValue: mockKeycloakService }
+      ]
     }).compileComponents();
   });
 
